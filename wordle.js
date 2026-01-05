3546cda0139101a2c57c5de15def6328e90c42ae
Pratyush Poudel <pratyushpoudel706@gmail.com>
2026-01-05 18:55:05 -0500

    exoskeleton built

---
+++ wordle.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const createSquares = () =>{
+
+        const gameBoard = document.getElementById("board");
+
+        for (let i = 0; i < 30; i++){
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.setAttribute("id", i + 1);
+            gameBoard.appendChild(square);
+        }
+    }
+
+    createSquares();
+})
